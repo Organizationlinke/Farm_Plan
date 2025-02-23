148b09b657256b5aa094ecb76fbec75549d679ac
AhmeGabr2010 <a.gabr26284@gmail.com>
2025-02-23 14:19:32 +0200

    Update to use dart-define

--- lib/Main_process.dart
+++ lib/Main_process.dart
@@ -16,8 +16,10 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
 
   final List<Map<String, dynamic>> areas =
       []; // نتائج الاستعلام من قاعدة البيانات
-  final List<Map<String, dynamic>> selectedAreas =
-      []; // العناصر اللي هيتم عرضها في row2
+  // final List<Map<String, dynamic>> selectedAreas =
+  //     []; // العناصر اللي هيتم عرضها في row2
+
+  final List<String> selectedAreas = []; // الآن القائمة تحتوي على نصوص فقط
 
   @override
   void initState() {
@@ -74,30 +76,60 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
     }
   }
 
+  // void selectArea(Map<String, dynamic> area) async {
+  //   if (!selectedAreas.contains(area)) {
+  //     Old_user_area = New_user_area;
+  //     New_user_area = area['farm_code'];
+  //     new_level++;
+  //     print(New_user_area);
+  //     checked();
+  //     await fetchAreas();
+
+  //     setState(() {
+  //       selectedAreas.add(area);
+
+  //     });
+  //   }
+  // }
   void selectArea(Map<String, dynamic> area) async {
-    if (!selectedAreas.contains(area)) {
-      Old_user_area=New_user_area;
-      New_user_area = area['farm_code'];
+    String areaCode = area['farm_code']; // استخراج النص من الخريطة
+
+    if (!selectedAreas.contains(areaCode)) {
+      Old_user_area = New_user_area;
+      New_user_area = areaCode;
+      String areaCode2= area[check_farm];
+      print(areaCode2);
       new_level++;
-      print(New_user_area);
+      
       checked();
+      
       await fetchAreas();
 
       setState(() {
-        selectedAreas.add(area);
+        selectedAreas.add(areaCode2); // إضافة النص بدلاً من الخريطة
       });
     }
   }
 
-  void removeArea(Map<String, dynamic> area) async{
-    
-      New_user_area = Old_user_area;
+  // void removeArea(Map<String, dynamic> area) async {
+  //   New_user_area = Old_user_area;
+  //   new_level--;
+  //   checked();
+  //   await fetchAreas();
+  //   setState(() {
+  //     selectedAreas.remove(area);
+
+  //   });
+  // }
+  void removeArea(String areaCode) async {
+    New_user_area = Old_user_area;
     new_level--;
     checked();
-      await fetchAreas();
+    await fetchAreas();
     setState(() {
-      selectedAreas.remove(area);
+      selectedAreas.remove(areaCode); // حذف العنصر النصي
     });
+    print(New_user_area);
   }
 
   @override
@@ -106,6 +138,7 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 204, 203, 203),
           // leading: Text(farm_title),
           // toolbarHeight: 1,
           title: Column(
@@ -121,12 +154,44 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
                   ),
                   Row(
                     children: [
+                      // SingleChildScrollView(
+                      //   scrollDirection: Axis.horizontal,
+                      //   child: Row(
+                      //     children: selectedAreas.map((area) {
+                      //       return Container(
+                      //         // margin: const EdgeInsets.symmetric(horizontal: 8),
+                      //         padding: const EdgeInsets.all(8),
+                      //         decoration: BoxDecoration(
+                      //           color: const Color.fromARGB(255, 228, 230, 228),
+                      //           borderRadius: BorderRadius.circular(8),
+                      //         ),
+                      //         child: Row(
+                      //           children: [
+                      //             Text(
+                      //               area[old_check_farm].toString(),
+                      //               style: const TextStyle(
+                      //                    fontSize: 12),
+                      //             ),
+                      //             const SizedBox(width: 4),
+                      //             GestureDetector(
+                      //               onTap: () => removeArea(area),
+                      //               child: const Icon(
+                      //                 Icons.close_rounded,
+                      //                 color: Colors.red,
+                      //                 size: 12,
+                      //               ),
+                      //             ),
+                      //           ],
+                      //         ),
+                      //       );
+                      //     }).toList(),
+                      //   ),
+                      // ),
                       SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Row(
-                          children: selectedAreas.map((area) {
+                          children: selectedAreas.map((areaCode) {
                             return Container(
-                              // margin: const EdgeInsets.symmetric(horizontal: 8),
                               padding: const EdgeInsets.all(8),
                               decoration: BoxDecoration(
                                 color: const Color.fromARGB(255, 228, 230, 228),
@@ -135,13 +200,12 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
                               child: Row(
                                 children: [
                                   Text(
-                                    area[old_check_farm].toString(),
-                                    style: const TextStyle(
-                                         fontSize: 12),
+                                    areaCode, // الآن يتم طباعة النص مباشرة
+                                    style: const TextStyle(fontSize: 12),
                                   ),
                                   const SizedBox(width: 4),
                                   GestureDetector(
-                                    onTap: () => removeArea(area),
+                                    onTap: () => removeArea(areaCode),
                                     child: const Icon(
                                       Icons.close_rounded,
                                       color: Colors.red,
@@ -159,63 +223,38 @@ class _MainProcessScreenState extends State<MainProcessScreen> {
                   )
                 ],
               ),
-              // SingleChildScrollView(
-              //   scrollDirection: Axis.horizontal,
-              //   child: Row(
-              //     children: areas.map((area) {
-              //       return GestureDetector(
-              //         onTap: () {
-              //           selectArea(area);
-              //         },
-              //         child: Container(
-              //           margin: const EdgeInsets.symmetric(horizontal: 8),
-              //           padding: const EdgeInsets.all(8),
-              //           decoration: BoxDecoration(
-              //             color: Colors.blueAccent,
-              //             borderRadius: BorderRadius.circular(8),
-              //           ),
-              //           child: Text(
-              //             area[check_farm].toString(),
-              //             style: const TextStyle(
-              //                 color: Colors.white, fontSize: 12),
-              //           ),
-              //         ),
-              //       );
-              //     }).toList(),
-              //   ),
-              // ),
             ],
           ),
         ),
         body: Row(
           children: [
-               SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-crossAxisAlignment: CrossAxisAlignment.start,
-mainAxisAlignment: MainAxisAlignment.start,
-                  children: areas.map((area) {
-                    return GestureDetector(
-                      onTap: () {
-                        selectArea(area);
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(vertical: 8),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.blueAccent,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Text(
-                          area[check_farm].toString(),
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 12),
-                        ),
+            Container(
+              color:  const Color.fromARGB(255, 204, 203, 203),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: areas.map((area) {
+                  return GestureDetector(
+                    onTap: () {
+                      selectArea(area);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(vertical: 8),
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(8),
                       ),
-                    );
-                  }).toList(),
-                ),
+                      child: Text(
+                        area[check_farm].toString(),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ),
+                  );
+                }).toList(),
               ),
+            ),
             Expanded(
               child: FutureBuilder(
                 future: fetchGroupedProcesses(),
@@ -223,14 +262,14 @@ mainAxisAlignment: MainAxisAlignment.start,
                   if (!snapshot.hasData) {
                     return const Center(child: CircularProgressIndicator());
                   }
-              
+
                   final processes = snapshot.data as List<Map<String, dynamic>>;
-              
+
                   return ListView.builder(
                     itemCount: processes.length,
                     itemBuilder: (context, index) {
                       final process = processes[index];
-              
+
                       return ListTile(
                         title: Text(process['process_name']),
                         onTap: () {
