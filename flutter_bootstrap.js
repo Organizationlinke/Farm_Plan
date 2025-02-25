66c88639f3b08df069e0487b61b2185392e7eefb
Organizationlinke <Organizationlinke@users.noreply.github.com>
2025-02-25 13:33:17 +0000

    deploy: 445c6e361ba951fbcc5c87c22c0711ddf9dae248

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1218234414"
+    serviceWorkerVersion: "3480490977"
   }
 });
